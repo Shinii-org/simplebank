ecf705dfc2271abb1e3281371fd6d2258c9310bd
Phu Vo <86593220+Shinii-org@users.noreply.github.com>
2023-05-06 21:24:46 +0700

    add user query

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,19 +6,24 @@ import (
 	"fmt"
 )
 
-type Store struct {
+type Store interface{
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store{
-	return &Store{
+func NewStore(db *sql.DB) Store{
+	return &SQLStore{
 		db: db,
 		Queries: New(db),
 	}
 }
 
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error{
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error{
 	tx, err := store.db.BeginTx(ctx, nil)
 
 	if err != nil{
@@ -55,7 +60,7 @@ var txKey = struct{}{}
 
 // TransferTx perform a money transfer from one account to another
 // It creates a transfer record, add account entries, and update accounts's balance within a single database transaction 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error){
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error){
 	var result TransferTxResult
 	// txName := ctx.Value(txKey)
 
